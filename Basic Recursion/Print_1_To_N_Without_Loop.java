b396ed79ee5e14251339681360cbf4e92d7bb6ef
Abhinav <abhinavmahalley22@gmail.com>
2024-06-23 22:20:37 +0530

    1st que ans

--- Basic Recursion/Print_1_To_N_Without_Loop.java
+++ Basic Recursion/Print_1_To_N_Without_Loop.java
@@ -3,8 +3,24 @@ import java.util.*;
 
 public class Print_1_To_N_Without_Loop {
 
-    public static void main(String[] args){
+    public static void print1toN(int N){
+        if(N < 1) return;
+
+        print1toN(N-1);
+        System.out.println(N);
         
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scn.nextInt();
+        print1toN(n);
+        
+        scn.close();
+
+
     }
     
 }
