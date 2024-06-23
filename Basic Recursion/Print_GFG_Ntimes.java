24032be8b27b9be648546ff02c5ca726494a869c
Abhinav <abhinavmahalley22@gmail.com>
2024-06-24 00:52:05 +0530

    3rd que

---
+++ Basic Recursion/Print_GFG_Ntimes.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Print_GFG_Ntimes {
+
+    public static void printGFG_Ntimes(int N){
+
+        //if N = 0 & 0<1 we return back
+        if(N < 1) return; 
+        
+        //Print GFG
+        System.out.println("GFG"); 
+
+        // then call recursive function ( Function which call it self)
+        printGFG_Ntimes(N-1);
+        
+    }
+
+    public static void main(String[] args){
+        
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scn.nextInt();
+
+        //Call to function
+        printGFG_Ntimes(n);
+        
+        scn.close();
+
+    }
+    
+}
