898c1316875749217bf6d9df7f763ee138d2f551
Abhinav <abhinavmahalley22@gmail.com>
2024-07-04 01:23:15 +0530

    fib

--- Basic Recursion/Fibonacci_Number.java
+++ Basic Recursion/Fibonacci_Number.java
@@ -5,8 +5,32 @@ public class Fibonacci_Number {
     public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n =  scn.nextInt(); //3 
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        //   1st  Basic Approach
+        
 
-        int n =  scn.nextInt();
+        if(n == 0 || n == 1){
+            System.out.println(n);
+        }
+
+        int fib0 = 0; // f[1] = 1 // f[2] = 1
+        int fib1 = 1; // f[2] = 1 // f[3] = 2
+    
+
+        for(int i = 2; i <= n; i++){ // f[2] = f[1] + f[0]  -> f[3] = f[2] + f[1]
+
+            int  temp = fib1 + fib0;  // 1+0 = 1 ->  1+1 = 2
+            fib0 = fib1; 
+            fib1 = temp;
+
+        }
+
+        System.out.println(fib1);
+
+        scn.close();
         
     }
 }
