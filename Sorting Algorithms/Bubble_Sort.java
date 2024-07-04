b1c13686d32209bcf1945702e4e916189f561be3
Abhinav <abhinavmahalley22@gmail.com>
2024-07-05 00:41:21 +0530

    buubble sort

--- Sorting Algorithms/Bubble_Sort.java
+++ Sorting Algorithms/Bubble_Sort.java
@@ -6,9 +6,9 @@ public class Bubble_Sort {
 
         for(int i = 0; i < n; i++){ // steps or passes
 
-            for(int j = 0; j < n-1 ; j++){
+            for(int j = 0; j < n-1 ; j++){ // To travers on array
 
-                if(arr[j] > arr[j+1]){
+                if(arr[j] > arr[j+1]){ // to compare between adjecent element
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
